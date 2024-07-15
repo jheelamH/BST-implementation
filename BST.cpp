9b347b7790afb65c69bb8cae2434c7e78badc954
Jheelam Hossain <jheelamhossain147@gmail.com>
2024-07-15 18:50:24 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -21,76 +22,9 @@ public:
     void case_a(node *, node *);
     void case_b(node *, node *);
     void case_c(node *, node *);
-    void preorder(node *);
-    void inorder(node *);
-    void postorder(node *);
-    void display(node *, int);
     node* getRoot() { return root; }
+    node* search(int);
 };
-
-int main() {
-    int choice, num;
-    BST bst;
-    node *temp;
-    while (1) {
-        cout << "Operations on BST" << endl;
-        cout << "1.Insert Element " << endl;
-        cout << "2.Delete Element " << endl;
-        cout << "3.Inorder Traversal" << endl;
-        cout << "4.Preorder Traversal" << endl;
-        cout << "5.Postorder Traversal" << endl;
-        cout << "6.Display" << endl;
-        cout << "7.Quit" << endl;
-        cout << "Enter your choice: ";
-        cin >> choice;
-        switch (choice) {
-            case 1:
-                temp = new node;
-                temp->left = temp->right = NULL;
-                cout << "Enter the number to be inserted: ";
-                cin >> temp->info;
-                if (bst.getRoot() == NULL) {
-                    bst.insert(NULL, temp);
-                } else {
-                    bst.insert(bst.getRoot(), temp);
-                }
-                break;
-            case 2:
-                if (bst.getRoot() == NULL) {
-                    cout << "Tree is empty" << endl;
-                    continue;
-                }
-                cout << "Enter the number to be deleted: ";
-                cin >> num;
-                bst.del(num);
-                break;
-            case 3:
-                cout << "Inorder Traversal " << endl;
-                bst.inorder(bst.getRoot());
-                cout << endl;
-                break;
-            case 4:
-                cout << "Preorder Traversal " << endl;
-                bst.preorder(bst.getRoot());
-                cout << endl;
-                break;
-            case 5:
-                cout << "Postorder Traversal " << endl;
-                bst.postorder(bst.getRoot());
-                cout << endl;
-                break;
-            case 6:
-                cout << "Display BST:" << endl;
-                bst.display(bst.getRoot(), 1);
-                cout << endl;
-                break;
-            case 7:
-                exit(1);
-            default:
-                cout << "Wrong choice" << endl;
-        }
-    }
-}
 
 void BST::find(int item, node **par, node **loc) {
     node *ptr, *ptrsave;
@@ -227,54 +161,63 @@ void BST::case_c(node *par, node *loc) {
     suc->right = loc->right;
 }
 
-void BST::preorder(node *ptr) {
-    if (root == NULL) {
-        cout << "Tree is empty" << endl;
-        return;
-    }
-    if (ptr != NULL) {
-        cout << ptr->info << "  ";
-        preorder(ptr->left);
-        preorder(ptr->right);
-    }
+node* BST::search(int item) {
+    node *parent, *location;
+    find(item, &parent, &location);
+    return location;
 }
 
-void BST::inorder(node *ptr) {
-    if (root == NULL) {
-        cout << "Tree is empty" << endl;
-        return;
-    }
-    if (ptr != NULL) {
-        inorder(ptr->left);
-        cout << ptr->info << "  ";
-        inorder(ptr->right);
-    }
-}
-
-void BST::postorder(node *ptr) {
-    if (root == NULL) {
-        cout << "Tree is empty" << endl;
-        return;
-    }
-    if (ptr != NULL) {
-        postorder(ptr->left);
-        postorder(ptr->right);
-        cout << ptr->info << "  ";
-    }
-}
-
-void BST::display(node *ptr, int level) {
-    int i;
-    if (ptr != NULL) {
-        display(ptr->right, level + 1);
-        cout << endl;
-        if (ptr == root)
-            cout << "Root->:  ";
-        else {
-            for (i = 0; i < level; i++)
-                cout << "       ";
+int main() {
+    int choice, num;
+    BST bst;
+    node *temp;
+    while (1) {
+        cout << "Operations on BST" << endl;
+        cout << "1.Insert Element " << endl;
+        cout << "2.Delete Element " << endl;
+        cout << "3.Search Element" << endl;
+        cout << "4.Quit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                temp = new node;
+                temp->left = temp->right = NULL;
+                cout << "Enter the number to be inserted: ";
+                cin >> temp->info;
+                if (bst.getRoot() == NULL) {
+                    bst.insert(NULL, temp);
+                } else {
+                    bst.insert(bst.getRoot(), temp);
+                }
+                break;
+            case 2:
+                if (bst.getRoot() == NULL) {
+                    cout << "Tree is empty" << endl;
+                    continue;
+                }
+                cout << "Enter the number to be deleted: ";
+                cin >> num;
+                bst.del(num);
+                break;
+            case 3:
+                if (bst.getRoot() == NULL) {
+                    cout << "Tree is empty" << endl;
+                    continue;
+                }
+                cout << "Enter the number to be searched: ";
+                cin >> num;
+                temp = bst.search(num);
+                if (temp != NULL) {
+                    cout << "Element " << num << " found in the tree." << endl;
+                } else {
+                    cout << "Element " << num << " not found in the tree." << endl;
+                }
+                break;
+            case 4:
+                exit(1);
+            default:
+                cout << "Wrong choice" << endl;
         }
-        cout << ptr->info;
-        display(ptr->left, level + 1);
     }
 }
